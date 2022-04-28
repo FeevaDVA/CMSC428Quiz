b2f76fdbc261cb481388923e3abad5c8cbc44a50
FeevaDVA <hvdwivedi@drdwivedi.com>
2022-04-28 17:01:12 -0400

    initial commit

--- CMSC428Quiz/ViewController.swift
+++ CMSC428Quiz/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("alive")
         // Do any additional setup after loading the view.
     }
 
